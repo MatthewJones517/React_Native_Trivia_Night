f07129c7ce4166d17c80a17f5823ffe2a0e9f465
Matthew Jones <mail@matthewrjones.me>
2021-04-04 13:32:09 -0400

    Create scaffold for header component

--- screens/Play.js
+++ screens/Play.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Text, SafeAreaView } from "react-native";
 
 import Wrapper from "../components/Wrapper";
+import Header from "../components/Header";
 
 const Play = () => {
   return (
     <Wrapper>
       <SafeAreaView>
-        <Text>Gameplay Screen</Text>
+        <Header />
       </SafeAreaView>
     </Wrapper>
   );
