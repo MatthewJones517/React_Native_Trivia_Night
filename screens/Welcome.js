9f2f6f2d0cb39edc86ea6392bc63e78642a7d7fd
Matthew Jones <mail@matthewrjones.me>
2021-04-04 12:41:26 -0400

    Added Roboto as app font

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, StyleSheet, Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
 
 import Wrapper from "../components/Wrapper";
