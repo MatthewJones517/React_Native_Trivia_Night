fbc7e118cabbd6b49788ffb426efaf9f8ea45496
Matthew Jones <mail@matthewrjones.me>
2021-04-04 13:48:36 -0400

    Added scoreboard scaffolding

---
+++ components/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Scoreboard = () => {
+  return (
+    <View>
+      <Text>Scoreboard</Text>
+    </View>
+  );
+};
+
+export default Scoreboard;
