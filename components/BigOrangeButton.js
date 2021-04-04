6e4b083a339a17cd9bd6b6bedc3af0610d46c426
Matthew Jones <mail@matthewrjones.me>
2021-04-04 11:56:25 -0400

    Created basic styling for big orange button

--- components/BigOrangeButton.js
+++ components/BigOrangeButton.js
@@ -1,8 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const BigOrangeButton = () => {
-  return <Text>This Will Be a Button!</Text>;
+  return (
+    <View>
+      <View style={styles.background} />
+      <View style={styles.foreground} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    width: 345,
+    height: 145,
+    backgroundColor: "#FF914D",
+    borderRadius: 30,
+  },
+  foreground: {
+    width: 340,
+    height: 140,
+    backgroundColor: "#FFDE59",
+    borderRadius: 30,
+    marginTop: -150,
+    marginLeft: -5,
+    zIndex: 100,
+  },
+});
 
 export default BigOrangeButton;
