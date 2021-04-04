bd463dd716907a1671141a777d04db7566c59075
Matthew Jones <mail@matthewrjones.me>
2021-04-04 10:51:16 -0400

    Added background gradient

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Welcome = () => {
   return (
     <View style={styles.container}>
+      <LinearGradient
+        colors={["rgba(0,0,0,0)", "rgba(0,0,0,0.8)"]}
+        style={styles.background}
+      />
       <Text>Welcome Screen</Text>
     </View>
   );
@@ -15,6 +20,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#4052B3",
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 500,
   },
 });
 
