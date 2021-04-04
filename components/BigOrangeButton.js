7cec38f822668a841795e2b75864fe16129049ff
Matthew Jones <mail@matthewrjones.me>
2021-04-04 11:38:25 -0400

    Create scaffold for the big orange button

---
+++ components/BigOrangeButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const BigOrangeButton = () => {
+  return <Text>This Will Be a Button!</Text>;
+};
+
+export default BigOrangeButton;
