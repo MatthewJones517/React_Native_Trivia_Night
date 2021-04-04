791efaef3235f6b17722bcd574fd806f9d9608ec
Matthew Jones <mail@matthewrjones.me>
2021-04-04 12:23:46 -0400

    Added Roboto as a Google Font

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { Image, StyleSheet, Text } from "react-native";
+import { useFonts } from "expo-font";
 
 import Wrapper from "../components/Wrapper";
 import BigOrangeButton from "../components/BigOrangeButton";
 
 const Welcome = () => {
-  return (
-    <Wrapper>
-      <Image source={require("../assets/logo.png")} style={styles.image} />
-      <BigOrangeButton width="350" height="150">
-        <Text>START GAME</Text>
-      </BigOrangeButton>
-    </Wrapper>
-  );
+  let [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("../assets/fonts/Roboto/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <View />;
+  } else {
+    return (
+      <Wrapper>
+        <Image source={require("../assets/logo.png")} style={styles.image} />
+        <BigOrangeButton width="350" height="150">
+          <Text style={styles.buttonText}>START GAME</Text>
+        </BigOrangeButton>
+      </Wrapper>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -21,6 +30,11 @@ const styles = StyleSheet.create({
     height: 224,
     resizeMode: "contain",
     marginBottom: 40,
+  },
+  buttonText: {
+    fontFamily: "Roboto-Bold",
+    fontSize: 36,
+    color: "#004AAD",
   },
 });
 
