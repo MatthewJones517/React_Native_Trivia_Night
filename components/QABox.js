54b5f49fbff6484492027683995cf6769b752ca2
Matthew Jones <mail@matthewrjones.me>
2021-04-04 16:34:38 -0400

    Added Question and Answer Box

---
+++ components/QABox.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+
+const QABox = () => {
+  let [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("../assets/fonts/Roboto/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Animal Planet is on its 11th version of this annual feature with a
+          different feel from the big football game
+        </Text>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffffff",
+    marginTop: 30,
+    padding: 20,
+    width: 350,
+    height: 350,
+    borderRadius: 30,
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 25,
+    color: "#004AAD",
+    fontFamily: "Roboto-Regular",
+  },
+});
+
+export default QABox;
--- screens/Play.js
+++ screens/Play.js
@@ -3,12 +3,14 @@ import { Text, SafeAreaView } from "react-native";
 
 import Wrapper from "../components/Wrapper";
 import Header from "../components/Header";
+import QABox from "../components/QABox";
 
 const Play = () => {
   return (
     <Wrapper>
       <SafeAreaView>
         <Header />
+        <QABox />
       </SafeAreaView>
     </Wrapper>
   );
