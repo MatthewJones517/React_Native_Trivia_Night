1a599498b0dcd7c21e254820dc079cfdfbbb5e82
Matthew Jones <mail@matthewrjones.me>
2021-04-04 16:40:00 -0400

    Added styling to SafeAreaView in Play screen to horizonally center elements

--- screens/Play.js
+++ screens/Play.js
@@ -1,19 +1,34 @@
 import React from "react";
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, StyleSheet } from "react-native";
 
 import Wrapper from "../components/Wrapper";
 import Header from "../components/Header";
 import QABox from "../components/QABox";
+import BigOrangeButton from "../components/BigOrangeButton";
 
 const Play = () => {
   return (
     <Wrapper>
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <Header />
         <QABox />
+        <BigOrangeButton
+          width="300"
+          height="125"
+          handlePress={() => console.log("pressed")}
+        >
+          <Text>REVEAL ANSWER</Text>
+          <Text>Point Value: 200</Text>
+        </BigOrangeButton>
       </SafeAreaView>
     </Wrapper>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+});
 
 export default Play;
