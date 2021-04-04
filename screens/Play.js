c288d8be7b8fd0497842d137d1293d134ddb1de9
Matthew Jones <mail@matthewrjones.me>
2021-04-04 16:50:47 -0400

    Added reveal answer button

--- screens/Play.js
+++ screens/Play.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Text, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
+import { useFonts } from "expo-font";
 
 import Wrapper from "../components/Wrapper";
 import Header from "../components/Header";
@@ -7,27 +13,52 @@ import QABox from "../components/QABox";
 import BigOrangeButton from "../components/BigOrangeButton";
 
 const Play = () => {
+  let [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("../assets/fonts/Roboto/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  } else {
+    return (
+      <Wrapper>
+        <SafeAreaView style={styles.container}>
+          <Header />
+          <QABox />
+          <RevealButton />
+        </SafeAreaView>
+      </Wrapper>
+    );
+  }
+};
+
+const RevealButton = () => {
   return (
-    <Wrapper>
-      <SafeAreaView style={styles.container}>
-        <Header />
-        <QABox />
-        <BigOrangeButton
-          width="300"
-          height="125"
-          handlePress={() => console.log("pressed")}
-        >
-          <Text>REVEAL ANSWER</Text>
-          <Text>Point Value: 200</Text>
-        </BigOrangeButton>
-      </SafeAreaView>
-    </Wrapper>
+    <BigOrangeButton
+      width="300"
+      height="125"
+      handlePress={() => console.log("pressed")}
+    >
+      <Text style={styles.revealButtonTextHead}>REVEAL ANSWER</Text>
+      <Text style={styles.revealButtonTextPoints}>Point Value: 200</Text>
+    </BigOrangeButton>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
+  },
+  revealButtonTextHead: {
+    color: "#004AAD",
+    fontSize: 25,
+    fontFamily: "Roboto-Bold",
+  },
+  revealButtonTextPoints: {
+    color: "#004AAD",
+    fontSize: 18,
+    fontFamily: "Roboto-Bold",
+    marginTop: 10,
   },
 });
 
