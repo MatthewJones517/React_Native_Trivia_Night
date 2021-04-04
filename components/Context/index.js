d724951d5a8bd0967a7efef69693fc08d4d093a7
Matthew Jones <mail@matthewrjones.me>
2021-04-04 17:54:46 -0400

    Added function to retrieve trivia questions from API.

--- components/Context/index.js
+++ components/Context/index.js
@@ -5,9 +5,48 @@ export const GameContext = React.createContext();
 export const Provider = (props) => {
   const [score, setScore] = useState(0);
   const [questions, setQuestions] = useState([]);
+  const [questionsLoading, setQuestionsLoading] = useState(false);
   const [isAnswerRevealed, setIsAnswerRevealed] = useState(false);
 
-  const handleNewGame = false;
+  const handleNewGame = async () => {
+    // Flip questions loading flag
+    setQuestionsLoading(true);
+
+    // Get 20 Questions from the Jeopardy API (http://jservice.io/)
+    const result = await fetch("http://jservice.io/api/random?count=20");
+
+    if (!result.ok) {
+      // If something went wrong, exit this function and report the
+      // issue to the user.
+      setQuestionsLoading(false);
+      return;
+    }
+
+    // Convert returned value to JSON
+    const returnedQuestions = await result.json();
+
+    // Filter out the questions that have been reported as Invalid
+    // (Require a picture, audio clip, etc...)
+    const filteredQuestions = returnedQuestions.filter(
+      (question) => question.invalid_count !== null
+    );
+
+    // Make sure we have at least 10 questions. If not, keep pinging the API
+    // until we have 10 good questions.
+    if (filteredQuestions.length < 10) {
+      handleNewGame();
+      return;
+    }
+
+    // Add the new questions to state
+    setQuestions(filteredQuestions);
+
+    // Flip the questions loading flag back.
+    setQuestionsLoading(false);
+
+    console.log(questions);
+  };
+
   const handleRevealAnswer = false;
   const handleUpdateScore = false;
 
@@ -17,6 +56,7 @@ export const Provider = (props) => {
         score,
         isAnswerRevealed,
         questions,
+        questionsLoading,
         actions: {
           updateScore: handleUpdateScore,
           revealAnswer: handleRevealAnswer,
