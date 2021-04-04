612a922135962eb40e474be3f8caa5d7158a8225
Matthew Jones <mail@matthewrjones.me>
2021-04-04 13:06:03 -0400

    Added basic navigation code to app.js

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 import Welcome from "./screens/Welcome";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <Welcome />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
