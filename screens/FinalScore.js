b0056fb476b09c06c46ae8bc0efea1c80bc94040
Matthew Jones <mail@matthewrjones.me>
2021-04-04 20:58:04 -0400

    Created scaffold for final score screen

---
+++ screens/FinalScore.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const FinalScore = () => {
+  return (
+    <View>
+      <Text>Final Score Screen</Text>
+    </View>
+  );
+};
+
+export default FinalScore;
