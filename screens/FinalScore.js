59664bf94583ad0ddfadbb256fa9af699fa1a807
Matthew Jones <mail@matthewrjones.me>
2021-04-04 21:17:54 -0400

    Updated styling for final score screen

--- screens/FinalScore.js
+++ screens/FinalScore.js
@@ -1,12 +1,63 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Image, StyleSheet, Text, ActivityIndicator, View } from "react-native";
+import { useFonts } from "expo-font";
 
-const FinalScore = () => {
+import Wrapper from "../components/Wrapper";
+import BigOrangeButton from "../components/BigOrangeButton";
+
+const FinalScore = ({ navigation }) => {
+  let [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("../assets/fonts/Roboto/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <View>
-      <Text>Final Score Screen</Text>
-    </View>
+    // Display Final Score Screen
+    <Wrapper>
+      <Image source={require("../assets/logo.png")} style={styles.image} />
+
+      <View style={styles.scoreContainer}>
+        <Text style={styles.scoreText}>FINAL SCORE:</Text>
+        <Text style={styles.scoreText}>5000</Text>
+      </View>
+
+      <BigOrangeButton
+        width="350"
+        height="150"
+        handlePress={() => {
+          navigation.navigate("Play");
+        }}
+      >
+        <Text style={styles.buttonText}>PLAY AGAIN</Text>
+      </BigOrangeButton>
+    </Wrapper>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 350,
+    height: 224,
+    resizeMode: "contain",
+    marginBottom: 40,
+  },
+  buttonText: {
+    fontFamily: "Roboto-Bold",
+    fontSize: 36,
+    color: "#004AAD",
+  },
+  scoreText: {
+    fontFamily: "Roboto-Bold",
+    fontSize: 50,
+    color: "#ffffff",
+  },
+  scoreContainer: {
+    alignItems: "center",
+    marginBottom: 40,
+  },
+});
 
 export default FinalScore;
